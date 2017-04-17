f9c243d36851f1e7f7a6c7866d34a2ca51acb36d
Joe Critchley <joecritch@gmail.com>
2017-04-17 23:57:58 +0100

    Added prettier to pre-commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Manchester React — Server
+
+## Deployment
+
+`now -e MEETUP_KEY=@meetup-key`
--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const srv = require('http').createServer(app);
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
 const queryString = require('query-string');
-const {RateLimiter} = require('limiter');
+const mcache = require('memory-cache');
+const { RateLimiter } = require('limiter');
 if (process.env.NODE_ENV === 'development') {
   require('dotenv').config();
 }
@@ -15,22 +16,32 @@ const limiter = new RateLimiter(15, 10000);
 function MeetupError(request) {
   this.status = request.status;
   this.statusText = request.statusText;
-  this.stack = (new Error()).stack;
+  this.stack = new Error().stack;
 }
 MeetupError.prototype = Object.create(Error.prototype);
 MeetupError.prototype.constructor = MeetupError;
 
-const MANCREACT_EVENT_ID = process.env.MEETUP_GROUP_ID;
-const MANCREACT_URL_NAME = process.env.MEETUP_URL_NAME;
+const MANCREACT_URL_NAME = 'Manchester-React-User-Group';
 const MEETUP_API_ROOT = 'https://api.meetup.com/';
 
-const meetupAPI = async (uri, args) => {
+const meetupAPI = async (uri, req, args) => {
   const noArgs = args == null;
-  let url = MEETUP_API_ROOT + uri + (noArgs ? '' : `?${queryString.stringify(args)}`)
-  url += (noArgs ? '?' : '&');
-  url += `key=${process.env.MEETUP_KEY}`;
-  const request = await fetch(url);
-  if (request.status !== 200) {
+  console.log('args', args);
+  let url =
+    MEETUP_API_ROOT + uri + (noArgs ? '' : `?${queryString.stringify(args)}`);
+  url += noArgs ? '?' : '&';
+  if (
+    req.get('X-Access-Token') != null && req.get('X-Access-Token') !== 'null'
+  ) {
+    url += `access_token=${req.get('X-Access-Token')}`;
+  } else {
+    url += `key=${process.env.MEETUP_KEY}`;
+  }
+  console.log('url', url);
+  const request = await fetch(url, {
+    method: req.method,
+  });
+  if (!request.ok) {
     throw new MeetupError(request);
     return;
   }
@@ -38,47 +49,124 @@ const meetupAPI = async (uri, args) => {
   return data;
 };
 
-app.options('*', cors()) // include before other routes
+app.options('*', cors()); // include before other routes
 app.use(cors());
 
 app.use((req, res, next) => {
   limiter.removeTokens(1, () => {
     next();
   });
-})
+});
 
-app.get('/group', async (req, res, next) => {
-  const response = await meetupAPI(MANCREACT_URL_NAME);
+const cache = (duration = 60) => {
+  return (req, res, next) => {
+    let key = '__express__' + req.originalUrl || req.url;
+    let cachedBody = mcache.get(key);
+    if (cachedBody) {
+      res.send(cachedBody);
+      return;
+    } else {
+      res.sendResponse = res.send;
+      res.send = body => {
+        mcache.put(key, body, duration * 1000);
+        res.sendResponse(body);
+      };
+      next();
+    }
+  };
+};
+
+app.get('/group', cache(), async (req, res) => {
+  const response = await meetupAPI(MANCREACT_URL_NAME, req);
   res.send(response);
 });
 
-app.get('/events', async (req, res, next) => {
+app.get('/events', cache(), async (req, res, next) => {
   try {
-    const response = await meetupAPI(MANCREACT_URL_NAME + '/events', {
-      status: 'past,upcoming,draft'
+    const response = await meetupAPI(MANCREACT_URL_NAME + '/events', req, {
+      status: 'past,upcoming,draft',
     });
     res.send(response);
-  } catch (e) {
-    res.status(e.status).send(e.statusText);
+  } catch (err) {
+    res.status(err.status).send(err.statusText);
   }
 });
 
-app.get('/events/:id', async (req, res, next) => {
-  res.send(await meetupAPI(MANCREACT_URL_NAME + `/events/${req.params.id}`, {
-    status: 'past,upcoming,draft'
-  }));
+app.get('/events/:id', cache(), async (req, res, next) => {
+  let response;
+  try {
+    response = await meetupAPI(
+      MANCREACT_URL_NAME + `/events/${req.params.id}`,
+      req,
+      {
+        status: 'past,upcoming,draft',
+      }
+    );
+  } catch (err) {
+    console.log(err);
+  }
+  res.send(response);
 });
 
 app.get('/events/:id/rsvps', async (req, res, next) => {
-  res.send(await meetupAPI(MANCREACT_URL_NAME + `/events/${req.params.id}/rsvps`, {
-    response: 'yes',
-  }));
+  res.send(
+    await meetupAPI(
+      MANCREACT_URL_NAME + `/events/${req.params.id}/rsvps`,
+      req,
+      {
+        response: 'yes',
+      }
+    )
+  );
+});
+
+app.post('/events/:id/rsvps', async (req, res, next) => {
+  try {
+    const response = await meetupAPI(
+      MANCREACT_URL_NAME + `/events/${req.params.id}/rsvps`,
+      req,
+      {
+        response: req.query.response,
+      }
+    );
+    res.send(response);
+  } catch (err) {
+    console.log('rsvp failed', err);
+    res.status(err.status).send(err.statusText);
+  }
 });
 
 app.get('/events/:id/comments', async (req, res, next) => {
-  res.send(await meetupAPI(MANCREACT_URL_NAME + `/events/${req.params.id}/comments`));
+  res.send(
+    await meetupAPI(
+      MANCREACT_URL_NAME + `/events/${req.params.id}/comments`,
+      req
+    )
+  );
 });
 
-srv.listen(3000, function () {
-  console.log('Listening on 3000');
+app.post('/events/:id/comments', async (req, res, next) => {
+  let response;
+  try {
+    response = await meetupAPI(
+      MANCREACT_URL_NAME + `/events/${req.params.id}/comments`,
+      req,
+      {
+        comment: req.query.comment,
+      }
+    );
+  } catch (err) {
+    console.log(err);
+  }
+  res.send(response);
+});
+
+app.get('/members/self', async (req, res, next) => {
+  res.send(await meetupAPI(`members/self`, req));
+});
+
+const port = process.env.PORT || 4000;
+
+srv.listen(port, function() {
+  console.log(`Listening on ${port}`);
 });
